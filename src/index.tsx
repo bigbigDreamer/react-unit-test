0b7bb6979ba3340bc5f22dd9de7039a225b6d3c4
jadeface.scholar <jadeface.scholar@gmail.com>
2020-10-26 22:09:28 +0800

    feat: 新增番外篇

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
 import Home from './pages/home'
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
-ReactDOM.render(<Home/>,
+ReactDOM.render( <DndProvider backend={HTML5Backend}><Home/></DndProvider>,
     document.getElementById('root')
 );
